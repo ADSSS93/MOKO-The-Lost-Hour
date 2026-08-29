52601c368f0c56e7a492112cb197b109dd090b10
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 10:14:19 +0200

    feat: add native PS1 audio module

---
+++ src/audio.h
@@ -0,0 +1,9 @@
+#ifndef MOKO_AUDIO_H
+#define MOKO_AUDIO_H
+
+void moko_audio_init(void);
+void moko_audio_sfx(int pitch);
+void moko_audio_set_room(int room);
+void moko_audio_tick(void);
+
+#endif
