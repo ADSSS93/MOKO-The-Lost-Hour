2b81ea24b41a121753fbc0b8be56c1496e4d0cc0
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 13:12:30 +0200

    feat: track NPC delivery stages

--- src/adventure.h
+++ src/adventure.h
@@ -5,7 +5,7 @@
 #include "world_events.h"
 #include "items.h"
 #include "journal.h"
-typedef struct { MokoQuests quests; MokoWorldEvents world; MokoInventory inventory; MokoJournal journal; int last_event; int last_reward; int notice_timer; int dash_count; int current_room; int room_start_health; int challenge_notice_quest; int challenge_notice_timer; uint8_t challenge_flags[5]; uint8_t npc_met[10]; } MokoAdventure;
+typedef struct { MokoQuests quests; MokoWorldEvents world; MokoInventory inventory; MokoJournal journal; int last_event; int last_reward; int notice_timer; int dash_count; int current_room; int room_start_health; int challenge_notice_quest; int challenge_notice_timer; uint8_t challenge_flags[5]; uint8_t npc_met[10]; uint8_t npc_delivered[10]; } MokoAdventure;
 void adventure_reset(MokoAdventure *a);
 void adventure_tick(MokoAdventure *a,int room,int shards,int echoes,int combo,int health,int focus);
 int adventure_interact(MokoAdventure *a,int room,int px,int py);
@@ -16,6 +16,8 @@ void adventure_challenge_zone(MokoAdventure *a,int room,int zone,int dashing);
 void adventure_challenge_position(MokoAdventure *a,int room,int px,int py,int dashing);
 int adventure_npc_talk(MokoAdventure *a,int npc_index);
 int adventure_npc_met(const MokoAdventure *a,int npc_index);
+int adventure_npc_delivered(const MokoAdventure *a,int npc_index);
+int adventure_npc_delivery_required(int npc_index);
 int adventure_npc_reward(const MokoAdventure *a);
 int adventure_completion(const MokoAdventure *a);
 const char *adventure_notice(const MokoAdventure *a);
