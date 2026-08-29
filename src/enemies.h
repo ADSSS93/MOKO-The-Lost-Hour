19348b73cefcb77f7c5eddc86a5694f5f1b6ce1c
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 14:23:37 +0200

    feat: track enemy defeats and room clears

--- src/enemies.h
+++ src/enemies.h
@@ -4,10 +4,13 @@
 typedef enum { ENEMY_TICKHOUND,ENEMY_RAINLING,ENEMY_SHADOW,ENEMY_GEARLING,ENEMY_WATCHER } MokoEnemyKind;
 typedef struct { short x,y,home_x,home_y; signed char vx,vy; uint8_t room,kind,hp,active,phase,hit_cooldown; } MokoEnemy;
 #define MOKO_ENEMY_COUNT 12
-typedef struct { MokoEnemy e[MOKO_ENEMY_COUNT]; } MokoEnemies;
+typedef struct { MokoEnemy e[MOKO_ENEMY_COUNT]; uint16_t defeats; uint8_t room_defeats[5]; } MokoEnemies;
 void enemies_reset(MokoEnemies *s);
 void enemies_tick(MokoEnemies *s,int room,int px,int py);
 int enemies_touch(const MokoEnemies *s,int room,int px,int py,int radius);
 int enemies_dash_hit(MokoEnemies *s,int room,int px,int py,int facing);
+int enemies_room_remaining(const MokoEnemies *s,int room);
+int enemies_room_cleared(const MokoEnemies *s,int room);
+int enemies_defeats(const MokoEnemies *s);
 const MokoEnemy *enemies_get(const MokoEnemies *s,int index);
 #endif
