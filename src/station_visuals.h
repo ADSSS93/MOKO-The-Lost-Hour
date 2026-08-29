6318c702ab10b98eca7f0241b90ed704476a23b8
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 19:32:34 +0200

    feat: add Silent Station visual module

---
+++ src/station_visuals.h
@@ -0,0 +1,6 @@
+#ifndef MOKO_STATION_VISUALS_H
+#define MOKO_STATION_VISUALS_H
+#include <stdint.h>
+#include <psxgpu.h>
+void station_visuals_draw(int anim_tick,uint32_t *ot,char **next_packet);
+#endif
