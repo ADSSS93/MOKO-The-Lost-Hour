a2c7bdfec2d2d55a2291245f00ef39fbd6d40e36
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 10:49:58 +0200

    feat: expose adventure world event helpers

--- src/adventure.h
+++ src/adventure.h
@@ -3,11 +3,14 @@
 #include <stdint.h>
 #include "quests.h"
 #include "world_events.h"
-typedef struct { MokoQuests quests; MokoWorldEvents world; int last_event; int last_reward; int notice_timer; int dash_count; } MokoAdventure;
+typedef struct { MokoQuests quests; MokoWorldEvents world; int last_event; int last_reward; int notice_timer; int dash_count; int current_room; } MokoAdventure;
 void adventure_reset(MokoAdventure *a);
 void adventure_tick(MokoAdventure *a,int room,int shards,int echoes,int combo,int health,int focus);
 int adventure_interact(MokoAdventure *a,int room,int px,int py);
 void adventure_dash(MokoAdventure *a);
 int adventure_completion(const MokoAdventure *a);
 const char *adventure_notice(const MokoAdventure *a);
+int adventure_near_event(const MokoAdventure *a,int room,int px,int py,int radius);
+int adventure_room_remaining(const MokoAdventure *a,int room);
+const MokoWorldEventDef *adventure_event(const MokoAdventure *a,int index);
 #endif
