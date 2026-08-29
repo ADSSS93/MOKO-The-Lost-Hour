f47454d48cc91ede4d3b5ad5910b7f385934ffc4
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 10:00:47 +0200

    fix: use PSn00bSDK uint32_t ordering table type

--- src/moko_sprite.h
+++ src/moko_sprite.h
@@ -1,7 +1,9 @@
 #ifndef MOKO_SPRITE_H
 #define MOKO_SPRITE_H
 
+#include <stdint.h>
+
 void moko_sprite_init(void);
-void moko_sprite_draw(int x, int y, int facing, int walk_tick, int invuln, int anim_tick, unsigned int *ot, char **next_packet);
+void moko_sprite_draw(int x, int y, int facing, int walk_tick, int invuln, int anim_tick, uint32_t *ot, char **next_packet);
 
 #endif
