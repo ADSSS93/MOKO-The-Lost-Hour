51b10dbb3bea979707a3f77b2f37011a715f5689
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 13:04:46 +0200

    feat: add enemy hit recovery

--- src/enemies.h
+++ src/enemies.h
@@ -2,7 +2,7 @@
 #define MOKO_ENEMIES_H
 #include <stdint.h>
 typedef enum { ENEMY_TICKHOUND,ENEMY_RAINLING,ENEMY_SHADOW,ENEMY_GEARLING,ENEMY_WATCHER } MokoEnemyKind;
-typedef struct { short x,y,home_x; signed char vx; uint8_t room,kind,hp,active,phase; } MokoEnemy;
+typedef struct { short x,y,home_x; signed char vx; uint8_t room,kind,hp,active,phase,hit_cooldown; } MokoEnemy;
 #define MOKO_ENEMY_COUNT 12
 typedef struct { MokoEnemy e[MOKO_ENEMY_COUNT]; } MokoEnemies;
 void enemies_reset(MokoEnemies *s);
