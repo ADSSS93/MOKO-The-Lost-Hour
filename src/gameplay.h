4eb7407a5bfc9e4e98814eb677219e630359a0ff
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 10:02:21 +0200

    feat: expand dash focus and combo gameplay API

--- src/gameplay.h
+++ src/gameplay.h
@@ -9,12 +9,21 @@ typedef struct {
     int combo_timer;
     int room_time;
     int deaths;
+    int echoes;
+    int shards;
+    int best_combo;
 } MokoGameplay;
 
 void gameplay_reset(MokoGameplay *g);
 void gameplay_tick(MokoGameplay *g, int moving);
 int gameplay_try_dash(MokoGameplay *g);
+int gameplay_move_speed(const MokoGameplay *g);
+int gameplay_dash_active(const MokoGameplay *g);
 void gameplay_reward(MokoGameplay *g, int amount);
+void gameplay_echo_collected(MokoGameplay *g);
+void gameplay_shard_collected(MokoGameplay *g);
+void gameplay_player_hurt(MokoGameplay *g);
 int gameplay_score_bonus(const MokoGameplay *g);
+int gameplay_focus_percent(const MokoGameplay *g);
 
 #endif
