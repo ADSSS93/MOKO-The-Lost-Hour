ab97710f026c8972fa1d0e2dac64ea381e65b3a9
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 10:43:13 +0200

    fix: align TIM loading with PSn00bSDK example

--- src/moko_sprite.c
+++ src/moko_sprite.c
@@ -7,7 +7,8 @@ static TIM_IMAGE moko_image;
 static int moko_ready=0;
 
 void moko_sprite_init(void){
-    if(GetTimInfo((const uint32_t *)moko_tim,&moko_image)!=0)return;
+    /* PSn00bSDK examples parse the TIM then upload its image/CLUT directly. */
+    GetTimInfo((const uint32_t *)moko_tim,&moko_image);
     LoadImage(moko_image.prect,moko_image.paddr);
     if(moko_image.mode&0x8)LoadImage(moko_image.crect,moko_image.caddr);
     DrawSync(0);
@@ -18,7 +19,8 @@ void moko_sprite_draw(int x,int y,int facing,int walk_tick,int invuln,int anim_t
     SPRT *spr;
     DR_TPAGE *page;
     int moving=(walk_tick>0);
-    int frame=moving ? 1+((walk_tick/7)&1) : 0;
+    int pose=moving ? 1+((walk_tick/7)&1) : 0;
+    int frame=(facing?0:3)+pose;
     int bob=moving ? ((walk_tick/7)&1) : ((anim_tick/24)&1);
     if(!moko_ready)return;
     if(invuln>0&&((anim_tick/3)&1))return;
@@ -28,7 +30,6 @@ void moko_sprite_draw(int x,int y,int facing,int walk_tick,int invuln,int anim_t
     setWH(spr,16,24);
     setUV0(spr,frame*16,0);
     setRGB0(spr,128,128,128);
-    (void)facing;
     addPrim(ot,spr);
     *next_packet+=sizeof(SPRT);
     page=(DR_TPAGE *)(*next_packet);
