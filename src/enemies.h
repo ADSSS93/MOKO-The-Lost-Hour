814a0716deace81ab8d9b7a3dfac5e5c83208534
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 18:04:50 +0200

    feat: add Chrono Mite enemy type

--- src/enemies.h
+++ src/enemies.h
@@ -1,9 +1,9 @@
 #ifndef MOKO_ENEMIES_H
 #define MOKO_ENEMIES_H
 #include <stdint.h>
-typedef enum { ENEMY_TICKHOUND,ENEMY_RAINLING,ENEMY_SHADOW,ENEMY_GEARLING,ENEMY_WATCHER,ENEMY_HOUR_WARDEN } MokoEnemyKind;
+typedef enum { ENEMY_TICKHOUND,ENEMY_RAINLING,ENEMY_SHADOW,ENEMY_GEARLING,ENEMY_WATCHER,ENEMY_HOUR_WARDEN,ENEMY_CHRONO_MITE } MokoEnemyKind;
 typedef struct { short x,y,home_x,home_y; signed char vx,vy; uint8_t room,kind,hp,active,phase,hit_cooldown; } MokoEnemy;
-#define MOKO_ENEMY_COUNT 13
+#define MOKO_ENEMY_COUNT 16
 typedef struct { MokoEnemy e[MOKO_ENEMY_COUNT]; uint16_t defeats; uint8_t room_defeats[5]; } MokoEnemies;
 void enemies_reset(MokoEnemies *s);
 void enemies_tick(MokoEnemies *s,int room,int px,int py);
