d7c7f04c8378b23f45a68f7f6db2369826ea9316
ADSSS93 <difiorea93@hotmail.com>
2026-08-29 11:51:10 +0200

    feat: expand world events into physical quest chains

--- src/world_events.h
+++ src/world_events.h
@@ -2,10 +2,10 @@
 #define MOKO_WORLD_EVENTS_H
 #include <stdint.h>
 #include "quests.h"
-#define MOKO_WORLD_EVENT_COUNT 24
+#define MOKO_WORLD_EVENT_COUNT 43
 typedef enum { WORLD_EVENT_PICKUP=0, WORLD_EVENT_INSPECT=1, WORLD_EVENT_SECRET=2 } MokoWorldEventKind;
 typedef struct { uint8_t collected[MOKO_WORLD_EVENT_COUNT]; uint8_t room_visits[5]; uint16_t interactions; } MokoWorldEvents;
-typedef struct { int room; int x; int y; int quest; int kind; const char *label; } MokoWorldEventDef;
+typedef struct { int room; int x; int y; int quest; int kind; int requires; const char *label; } MokoWorldEventDef;
 void world_events_reset(MokoWorldEvents *w);
 void world_events_enter_room(MokoWorldEvents *w,int room,MokoQuests *q);
 int world_events_interact(MokoWorldEvents *w,MokoQuests *q,int room,int x,int y,int radius);
