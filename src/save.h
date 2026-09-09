b81527dabbd5a0178a66df0e9a9b0fa7eb1a924b
ADSSS93 <difiorea93@hotmail.com>
2026-09-10 00:14:57 +0200

    feat: expose checksum refresh for runtime snapshots

--- src/save.h
+++ src/save.h
@@ -43,6 +43,7 @@ typedef struct {
 
 void moko_save_defaults(MokoSave *save);
 int moko_save_validate(const MokoSave *save);
+void moko_save_refresh(MokoSave *save);
 void moko_save_record_clear(MokoSave *save,int score,int combo,int echoes,int deaths);
 void moko_save_set_checkpoint(MokoSave *save,int room,int shards,int score,int timer,uint8_t puzzle_mask,uint8_t shard_mask,uint8_t echo_mask,uint8_t switch_mask);
 void moko_save_clear_checkpoint(MokoSave *save);
