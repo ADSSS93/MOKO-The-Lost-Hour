81d14f71ed9201643f10b69b5f9c80c72d83f212
ADSSS93 <difiorea93@hotmail.com>
2026-09-09 22:18:54 +0200

    feat: persist shard checkpoints in memory-card save v2

--- src/save.h
+++ src/save.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define MOKO_SAVE_MAGIC 0x4d4f4b4fU
-#define MOKO_SAVE_VERSION 1
+#define MOKO_SAVE_VERSION 2
 
 typedef struct {
     uint32_t magic;
@@ -15,12 +15,23 @@ typedef struct {
     uint16_t best_combo;
     uint16_t best_echoes;
     uint16_t best_deaths;
-    uint16_t reserved;
+    uint8_t has_checkpoint;
+    uint8_t checkpoint_room;
+    uint8_t checkpoint_shards;
+    uint8_t puzzle_mask;
+    uint8_t shard_mask;
+    uint8_t echo_mask;
+    uint8_t switch_mask;
+    uint8_t reserved;
+    uint32_t checkpoint_score;
+    uint32_t checkpoint_time;
 } MokoSave;
 
 void moko_save_defaults(MokoSave *save);
 int moko_save_validate(const MokoSave *save);
 void moko_save_record_clear(MokoSave *save,int score,int combo,int echoes,int deaths);
+void moko_save_set_checkpoint(MokoSave *save,int room,int shards,int score,int timer,uint8_t puzzle_mask,uint8_t shard_mask,uint8_t echo_mask,uint8_t switch_mask);
+void moko_save_clear_checkpoint(MokoSave *save);
 uint16_t moko_save_checksum(const MokoSave *save);
 
 #endif
